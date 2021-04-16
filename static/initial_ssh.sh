1790817eac70589e70f39a21b354d7a8042e2990
zinglax <dylanzingler@gmail.com>
2021-04-16 01:00:24 -0400

    Updated the initial_ssh file, added ssh-add

--- static/initial_ssh.sh
+++ static/initial_ssh.sh
@@ -2,7 +2,11 @@
 # Creates an initial key and config file.
 
 SSH_KEY="$(hostname)_initial"
+
+# Create RSA SSH Key
 ssh-keygen -q -t rsa -N '' -f ${HOME}/.ssh/${SSH_KEY} <<<y 2>&1 >/dev/null
+
+# Populate the config file
 touch ${HOME}/.ssh/config
 echo "IdentityFile ${HOME}/.ssh/${SSH_KEY}" >> ${HOME}/.ssh/config
 echo "" >> ${HOME}/.ssh/config
@@ -10,3 +14,6 @@ echo "Host gitlab.billzingler.com" >> ${HOME}/.ssh/config
 echo "User gitlab" >> ${HOME}/.ssh/config
 echo "Port 2022" >> ${HOME}/.ssh/config
 echo "IdentityFile ${HOME}/.ssh/${SSH_KEY}" >> ${HOME}/.ssh/config
+
+# Adds the ssh key to keychain
+ssh-add ${SSH_KEY}
